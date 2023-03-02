d5162cfbe7e0aec10ce8ceeee3d27b4bc7a14b39
milhamap <ilham3pratama@gmail.com>
2023-03-02 09:43:27 +0700

    Fiz Bab 2 Percobaan 3

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
         _counter = 1;
       }
 
-      _counter % 2 == 0 ? _text = "Genap" : _text = "Ganjil";
+      for (int i = 0; i <= _counter; i++) {
+        if (i % 2 != 0) {
+          _text != '${i}, ';
+        }
+      }
     });
   }
 
